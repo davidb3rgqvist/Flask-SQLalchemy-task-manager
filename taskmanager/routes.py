ef186c7fbfaf9e4faf99128c3f58fc1f12575820
David Bergqvist <davidbergqvist@MBPsomthorDavid-2.lan>
2024-04-24 11:48:38 +0200

    collapsiable tasks

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -2,17 +2,24 @@ from flask import render_template, request, redirect, url_for
 from . import app, db
 from taskmanager.templates.models import Category, Task
 
+class Category(db.Model):
+    __tablename__ = 'categories'
+
+    id = db.Column(db.Integer, primary_key=True)
+    category_name = db.Column(db.String(100), nullable=False)
+
+    def __str__(self):
+        return self.category_name
 
 @app.route("/")
 def home():
-    return render_template("tasks.html")
-
+    tasks = list(Task.query.order_by(Task.id).all())
+    return render_template("tasks.html", tasks=tasks)
 
 @app.route("/categories")
 def categories():
     categories = list(Category.query.order_by(Category.category_name).all())
     return render_template("categories.html", categories=categories)
-
 
 @app.route("/add_category", methods=["GET", "POST"])
 def add_category():
@@ -23,8 +30,7 @@ def add_category():
         return redirect(url_for("categories"))
     return render_template("add_category.html")
 
-
-@app.route("/edit_categor/<int:category_id>", methods=["GET", "POST"])
+@app.route("/edit_category/<int:category_id>", methods=["GET", "POST"])
 def edit_category(category_id):
     category = Category.query.get_or_404(category_id)
     if request.method == "POST":
@@ -32,3 +38,26 @@ def edit_category(category_id):
         db.session.commit()
         return redirect(url_for("categories"))
     return render_template("edit_category.html", category=category)
+
+@app.route("/delete_category/<int:category_id>")
+def delete_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    db.session.delete(category)
+    db.session.commit()
+    return redirect(url_for("categories"))
+
+@app.route("/add_task", methods=["GET", "POST"])
+def add_task():
+    categories = list(Category.query.order_by(Category.category_name).all())
+    if request.method == "POST":
+        task = Task(
+            task_name=request.form.get("task_name"),
+            task_description=request.form.get("task_description"),
+            is_urgent=bool(True if request.form.get("is_urgent")else False),
+            due_date=request.form.get("due_date"),
+            category_id=request.form.get("category_id")
+        )
+        db.session.add(task)
+        db.session.commit()
+        return redirect(url_for("home"))
+    return render_template("add_task.html", categories=categories)
